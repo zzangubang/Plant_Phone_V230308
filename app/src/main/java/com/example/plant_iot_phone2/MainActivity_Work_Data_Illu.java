9d3bbf368920bd74a39bee3a3a994dfbee3676e1
bangsh <bangsh.ahjoo@gmail.com>
2023-07-07 16:46:51 +0900

    20230707v

--- app/src/main/java/com/example/plant_iot_phone2/MainActivity_Work_Data_Illu.java
+++ app/src/main/java/com/example/plant_iot_phone2/MainActivity_Work_Data_Illu.java
@@ -50,9 +50,9 @@ public class MainActivity_Work_Data_Illu extends Fragment {
     TableLayout table;
 
     GetDate gDate;
-    String getDateURL = "http://aj3dlab.dothome.co.kr/Test_valueDate_Android.php";
+    String getDateURL = "http://hosting.ajplants.com/Plant_valueDate_Android.php";
     GetValue gValue;
-    String getValueURL = "http://aj3dlab.dothome.co.kr/Test_illuTable_Android.php";
+    String getValueURL = "http://hosting.ajplants.com/Plant_illuTable_Android.php";
 
     ProgressDialog dialog;
     Toast toast;
@@ -131,7 +131,7 @@ public class MainActivity_Work_Data_Illu extends Fragment {
                 String selectDate = year + "-" + monthS + "-" + dayOfMonthS;
 
                 dateT.setText(selectDate);
-                webView.loadUrl("http://aj3dlab.dothome.co.kr/Plant_illuGraph.php?date="+selectDate+"&model="+model);
+                webView.loadUrl("http://hosting.ajplants.com/Plant_illuGraph.php?date="+selectDate+"&model="+model);
                 dialog.show();
                 gValue = new GetValue();
                 gValue.execute(getValueURL, selectDate);
@@ -232,7 +232,7 @@ public class MainActivity_Work_Data_Illu extends Fragment {
                 }
 
                 dateT.setText(date);
-                webView.loadUrl("http://aj3dlab.dothome.co.kr/Plant_illuGraph.php?date="+date+"&model="+model);
+                webView.loadUrl("http://hosting.ajplants.com/Plant_illuGraph.php?date="+date+"&model="+model);
                 gValue = new GetValue();
                 gValue.execute(getValueURL, date);
 
--- app/src/main/java/com/example/plant_iot_phone2/MainActivity_Work_Data_Temp.java
+++ app/src/main/java/com/example/plant_iot_phone2/MainActivity_Work_Data_Temp.java
@@ -50,9 +50,9 @@ public class MainActivity_Work_Data_Temp extends Fragment {
     TableLayout table;
 
     GetDate gDate;
-    String getDateURL = "http://aj3dlab.dothome.co.kr/Test_valueDate_Android.php";
+    String getDateURL = "http://hosting.ajplants.com/Plant_valueDate_Android.php";
     GetValue gValue;
-    String getValueURL = "http://aj3dlab.dothome.co.kr/Test_tempTable_Android.php";
+    String getValueURL = "http://hosting.ajplants.com/Plant_tempTable_Android.php";
 
     ProgressDialog dialog;
     Toast toast;
@@ -129,7 +129,7 @@ public class MainActivity_Work_Data_Temp extends Fragment {
                 String selectDate = year + "-" + monthS + "-" + dayOfMonthS;
 
                 dateT.setText(selectDate);
-                webView.loadUrl("http://aj3dlab.dothome.co.kr/Plant_tempGraph.php?date="+selectDate+"&model="+model);
+                webView.loadUrl("http://hosting.ajplants.com/Plant_tempGraph.php?date="+selectDate+"&model="+model);
                 dialog.show();
                 gValue = new GetValue();
                 gValue.execute(getValueURL, selectDate);
@@ -230,7 +230,7 @@ public class MainActivity_Work_Data_Temp extends Fragment {
                 }
 
                 dateT.setText(date);
-                webView.loadUrl("http://aj3dlab.dothome.co.kr/Plant_tempGraph.php?date="+date+"&model="+model);
+                webView.loadUrl("http://hosting.ajplants.com/Plant_tempGraph.php?date="+date+"&model="+model);
                 gValue = new GetValue();
                 gValue.execute(getValueURL, date);
 
